4ce54477ac9afb232610e6ba3c346f45f845dd85
Arnaldo Perez <arnaldoeperezb@gmail.com>
2022-11-02 19:46:35 -0400

    Inicio del workshop

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,6 @@
 import functions from "firebase-functions"
 import myApi from "./api.js"
-// +import { keysApi } from "./crypto.js";
 
 const api = functions.https.onRequest(myApi);
-// const apiKeys = functions.https.onRequest(keysApi);
 
 export {api}
